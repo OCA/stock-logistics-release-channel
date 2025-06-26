b1efa8efa12650fb7d515ed847516107e2a02bdb
Ruchir Shukla <ruchir@bizzappdev.com>
2025-06-26 13:26:11 +0530

    [MIG] stock_release_channel_auto_release: Migration to 18.0

--- stock_release_channel_auto_release/__manifest__.py
+++ stock_release_channel_auto_release/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Release Channel Auto Release",
     "summary": """
         Add an automatic release mode to the release channel""",
-    "version": "16.0.1.1.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-release-channel",
